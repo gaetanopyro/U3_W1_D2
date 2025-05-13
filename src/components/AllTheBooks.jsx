5f30af3dfbd0e831db631b6475f22239fc4a9a9f
gaetanopyro <gaetano.miscio@gmail.com>
2025-05-13 17:15:52 +0200

    add point 6 pt2

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -9,15 +9,16 @@ class AllTheBooks extends Component {
       <Container fluid>
         <Row className="justify-content-center">
           <Col xs={10} className="text-center">
-            <Card>
-              {fantasy.map}
-              <Card.Img variant="top" src="https://images-na.ssl-images-amazon.com/images/I/51eHtkVLL5L.jpg" />
-              <Card.Body>
-                <Card.Title>The Last Wish: Introducing the Witcher</Card.Title>
-                <Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
+            {fantasy.map((book) => (
+              <Card>
+                <Card.Img variant="top" src={book.img} />
+                <Card.Body>
+                  <Card.Title>{book.title}</Card.Title>
+                  <Card.Text>{book.category}</Card.Text>
+                  <Button variant="primary">{book.price}</Button>
+                </Card.Body>
+              </Card>
+            ))}
           </Col>
         </Row>
       </Container>
